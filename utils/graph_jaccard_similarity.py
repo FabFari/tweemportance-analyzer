d1e71a0636dd2b82b34f4493ab2fb59804661289
FabFari <fabriziofarinacci@gmail.com>
2017-02-06 10:18:41 +0100

    Added file reader/writer for bitmask

--- utils/graph_jaccard_similarity.py
+++ utils/graph_jaccard_similarity.py
@@ -1,4 +1,6 @@
+import os
 from BitVector import BitVector
+
 
 # G = {'N1': ['N2',..., 'Nl'], ..., 'Nm': ['N3',..., 'Nn']}
 def compare_graphs(g1, g2):
@@ -12,9 +14,23 @@ def compare_graphs(g1, g2):
     return True
 
 
+def bitmask_file_parser(filename="hashtags_bitmasks.tsv", path="..\\data"):
+    ht_bitmask = dict()
+
+    with open(os.path.join(path, filename), 'r') as f:
+        for line in f:
+            hashtag = line.strip("\n").split("\t")
+            ht_bitmask[hashtag[0]] = BitVector(bitstring=hashtag[1])
+
+    return ht_bitmask
+
+
 # J(A, B) = |A *intersect* B| / |A *union* B|
 # H = [{'N1': BitVector1, ..., 'Nm': BitVectorm}, ...]
-def graph_jaccard_similarity(hashtags, mask_size):
+def graph_jaccard_similarity(hashtags, mask_size=None):
+    if mask_size is None:
+        mask_size = len(hashtags.values()[0])
+
     union_bitmask = BitVector(intVal=0, size=mask_size)
     intersec_bitmask = ~BitVector(intVal=0, size=mask_size)
 
@@ -23,4 +39,10 @@ def graph_jaccard_similarity(hashtags, mask_size):
         intersec_bitmask &= bm
 
     return float(intersec_bitmask.count_bits()) / union_bitmask.count_bits()
+
+if __name__ == "__main__":
+    ht_bitmask = bitmask_file_parser()
+    for (ht, bm) in ht_bitmask.iteritems():
+        print "{}: {}".format(ht, bm)
+    graph_jaccard_similarity(ht_bitmask)
 
