6a1ba4298aab1ad470bfd49336fea105c028d7d1
FabFari <fabriziofarinacci@gmail.com>
2017-02-10 19:44:52 +0100

    Added get_source_tweets and get_replies

--- twitter_data_retrieve/twitter_data_retriever.py
+++ twitter_data_retrieve/twitter_data_retriever.py
@@ -4,14 +4,17 @@ import twitter
 from tweepy import *
 
 
-NOME = "realDonaldTrump"
+NOME = "matteosalvinimi"
 FILE = "tweets.txt"
-FILE_ANSWERS = "answers.txt"
+FILE_REPLIES = "replies.txt"
+
+NUM_TWEETS = 100
 
 access_token = "815961135321059330-7i5Mh5wC2q6WNJJiJMrlqD6k3m9DRMm"
 access_token_secret = "9yDVE9SlD1qwHNPcew3mxTysYgU7onmEVmFvpnWsKr844"
 consumer_key = "kZEJIcJ5t9FNGWhMEkm8WyEV7"
 consumer_secret = "oIsyfHv8wbxsHcmvMXAOLkv4HzuoR09ii1fANUHXMnO8JEVfmn"
+
 
 class StdOutListener(StreamListener):
     """A basic listener that just prints received tweets to stdout."""
@@ -23,24 +26,23 @@ class StdOutListener(StreamListener):
         print status
 
 
-
-
 def setup():
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    #listener = StdOutListener()
-    #stream = Stream(auth, listener)
+    # listener = StdOutListener()
+    # stream = Stream(auth, listener)
 
     # Construct the API instance
     api = tweepy.API(auth)
     return api
 
+
 def setup_2():
     api = twitter.Api(consumer_key=consumer_key,
-                     consumer_secret=consumer_secret,
-                     access_token_key=access_token,
-                     access_token_secret=access_token_secret)
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token,
+                      access_token_secret=access_token_secret)
     dict = {}
     rep = api.GetSearch(raw_query="q=salvini&count=100&page=1")
     for r in rep:
@@ -52,42 +54,105 @@ def setup_2():
 
     for k in dict.keys():
         print k, ": ", dict[k]
-    #replies = (q="to:{}".format(NOME), count=100, page=15)
-    #print "Replies: ", len(replies)
+    # replies = (q="to:{}".format(NOME), count=100, page=15)
+    # print "Replies: ", len(replies)
+
 
 def get_reply(stream):
     stream.filter()
 
 
+def get_source_tweets(api, screenname=NOME):
+    tweets_retrieved = 0
 
-def get_tweets(api,screenname=NOME):
-    status_set = api.user_timeline(screen_name=NOME)
+    first_try = True
+    max_id = None
+    min_id = None
 
-    with open(FILE, 'wt') as f, open(FILE_ANSWERS, "wt") as f_a:
+    with open(FILE, 'wt') as f:
 
-        for status in status_set:
+        while tweets_retrieved < NUM_TWEETS:
+            try:
+                if not first_try:
+                    status_set = api.user_timeline(screen_name=NOME, count=100, max_id=max_id)
+                else:
+                    status_set = api.user_timeline(screen_name=NOME, count=100)
+            except tweepy.error.TweepError as err:
+                print err
+                break
 
-            print status.id
-            print status.text
-            print status.in_reply_to_status_id
-            user = status.author
-            print user.screen_name
-            #replies = api.search(q="to:{}".format(NOME), sinceId = status.id)
-            #replies = tweepy.Cursor(api.search(q="to:{}".format(NOME), count=1500, page=1)
-            #print "Replies: ", len(replies)
-            count = 0
-            dict = {}
-            for s in tweepy.Cursor(api.search, q="to:{}".format(NOME), since_id=status.id).items():
-                if dict.has_key(s.id):
-                    return
-                else :
-                    dict[s.id] = 1
-                    print "Replie: ", s.id, " " , s.text
-                    #f.write("id:{}\ntext:{}\n\n".format(s.id, s.text))
-                    count+=1
-                #f_a.write("{}:{}\n\n".format(status.id, a))
-            #f.write("id:{}\n\n".format(status))#.format(t['id'], t['text']))
+            print "<!----NUM_TWEETS:", len(status_set)
+
+            # RETRIEVE ALL LEVEL 0 TWEETS
+            first_tweet = True
+            for status in status_set:
+                if first_tweet:
+                    min_id = status.id
+                    first_tweet = False
+
+                if status.in_reply_to_status_id is not None:
+                    continue
+
+                if len(status.entities[u'hashtags']) == 0:
+                    continue
+
+                # SALVINI TEST
+                if status.entities[u'hashtags'][0][u'text'] == u'Salvini' and len(status.entities[u'hashtags']) < 2:
+                    continue
+
+                if min_id > status.id:
+                    min_id = status.id
+
+                line = "{}\n{}\n".format(status.id, status.text.encode('utf-8').replace("\n", " "))
+                f.write(line)
+                print line
+
+                tweets_retrieved += 1
+
+                if tweets_retrieved >= NUM_TWEETS:
+                    break
+
+            max_id = min_id
+            # TODO
+            # Create file per tweet
+    return min_id
+
+
+def get_replies(screen_name=NOME, min_id=None, max_replies=-1, filename=FILE_REPLIES):
+    ids_set = set()
+
+    with open(filename, "wt") as f:
+        try:
+            if min_id is None:
+                replies = tweepy.Cursor(api.search, q="to:{}".format(screen_name))
+            else:
+                replies = tweepy.Cursor(api.search, q="to:{}".format(screen_name), since_id=min_id)
+
+            for s in replies.items():
+                if s.in_reply_to_status_id is None:
+                    continue
+
+                if id in ids_set:
+                    break
+                else:
+                    ids_set.add(s.id)
+                    line = "{}\t{}\t{}\t{}\n\n".format(s.id, s.in_reply_to_status_id, s.author.screen_name, s.text.encode('utf-8').replace("\n", " "))
+                    print line
+                    f.write(line)
+                    if max_replies != -1 and len(ids_set) >= max_replies:
+                        break
+        except tweepy.error.TweepError as err:
+            print err
+        finally:
+            print "<------Tweets retrieved: ", len(ids_set)
+
+
+def tweet_graph_builder(file_tweets=FILE):
+    # TODO
+    return None
+
 
 if __name__ == "__main__":
     api = setup()
-    get_tweets(api)
+    # get_source_tweets(api)
+    get_replies()
